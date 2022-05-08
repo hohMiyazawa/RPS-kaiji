72d97f43d51dad33902bb81f925f2605b3756488
hohMiyazawa <sbrendberg@yandex.com>
2022-05-08 13:13:34 +0200

    context modelling

--- rps.js
+++ rps.js
@@ -86,6 +86,7 @@ function rps_evaluate(r,p,s){
 }
 
 function bot_pick(){
+	return bot_pick2();
 	let strategy_index = Math.floor(Math.random()*5);
 	if(strategy_index === 0){
 		return Math.floor(Math.random()*3);
@@ -117,6 +118,182 @@ function bot_pick(){
 	return bot_pick();//debug
 }
 
+let stats = {
+	prev: Math.floor(Math.random()*3),//initial random previous
+	prev_bot: Math.floor(Math.random()*3),//initial random previous
+	level_0: new Array(3).fill(1),
+	level_1: new Array(9).fill(1),
+	level_2: new Array(27).fill(1),
+	level_3: new Array(81).fill(1),
+	self_1: new Array(9).fill(1),
+	self_2: new Array(27).fill(1),
+	self_3: new Array(81).fill(1),
+	history: [],
+	rounds: 0,
+}
+
+function update_stats(pick,bot){
+	stats.history.push(bot);
+	stats.history.push(pick);
+	stats.level_0[pick]++;
+	stats.level_1[pick + bot*3]++;
+	stats.prev = pick;
+	stats.prev_bot = bot;
+	stats.rounds++;
+	if(stats.history.length > 2){
+		stats.level_2[pick + bot*3 + stats.history[stats.history.length - 3] * 9]++;
+		stats.self_1[pick + stats.history[stats.history.length - 3] * 3]++;
+	}
+	if(stats.history.length > 4){
+		stats.level_2[pick + bot*3 + stats.history[stats.history.length - 3] * 9 + stats.history[stats.history.length - 4] * 27]++;
+		stats.self_2[pick + stats.history[stats.history.length - 3] * 3 + stats.history[stats.history.length - 5] * 9]++;
+	}
+	if(stats.history.length > 6){
+		stats.self_3[pick + stats.history[stats.history.length - 3] * 3 + stats.history[stats.history.length - 5] * 9 + stats.history[stats.history.length - 7] * 27]++;
+	}
+}
+
+let models = [
+	{
+		name: "random",
+		weight: 1,
+		mod: function(){
+			let chances = [1/3,1/3,1/3];
+			return chances;
+		}
+	},
+	{
+		name: "level_0",
+		weight: 1,
+		mod: function(){
+			let index = 0;
+			let r = stats.level_0[index + 0];
+			let p = stats.level_0[index + 1];
+			let s = stats.level_0[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "level_1",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev_bot * 3;
+			let r = stats.level_1[index + 0];
+			let p = stats.level_1[index + 1];
+			let s = stats.level_1[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "level_2",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev_bot * 3 + stats.prev * 9;
+			let r = stats.level_2[index + 0];
+			let p = stats.level_2[index + 1];
+			let s = stats.level_2[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "level_3",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev_bot * 3 + stats.prev * 9 + (stats.history[stats.history.length - 3] || 0) * 27;
+			let r = stats.level_3[index + 0];
+			let p = stats.level_3[index + 1];
+			let s = stats.level_3[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "self_1",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev * 3;
+			let r = stats.self_1[index + 0];
+			let p = stats.self_1[index + 1];
+			let s = stats.self_1[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "self_2",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev * 3 + (stats.history[stats.history.length - 3] || 0) * 9;
+			let r = stats.self_2[index + 0];
+			let p = stats.self_2[index + 1];
+			let s = stats.self_2[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+	{
+		name: "self_3",
+		weight: 1,
+		mod: function(){
+			let index = stats.prev * 3 + (stats.history[stats.history.length - 3] || 0) * 9 + (stats.history[stats.history.length - 5] || 0) * 27;
+			let r = stats.self_3[index + 0];
+			let p = stats.self_3[index + 1];
+			let s = stats.self_3[index + 2];
+			let count = r + p + s;
+			let chances = [r/count,p/count,s/count];
+			return chances;
+		}
+	},
+]
+
+function bot_pick2(){
+	let r_score = 0;
+	let p_score = 0;
+	let s_score = 0;
+	models.forEach(model => {
+		let pred = model.mod();
+		r_score += (pred[2] - pred[1]) * model.weight;
+		p_score += (pred[0] - pred[2]) * model.weight;
+		s_score += (pred[1] - pred[0]) * model.weight;
+	});
+	return shuffle([
+		{type: 0,val: r_score},
+		{type: 1,val: p_score},
+		{type: 2,val: s_score},
+	]).sort(
+		(b,a) => a.val - b.val
+	)[0].type
+}
+
+function update_models(pick){
+	let r_prob = 0;
+	let p_prob = 0;
+	let s_prob = 0;
+	models.forEach(model => {
+		let pred = model.mod();
+		r_prob += pred[0] * model.weight;
+		p_prob += pred[1] * model.weight;
+		s_prob += pred[2] * model.weight;
+	});
+	let sum = r_prob + p_prob + s_prob;
+	let corr_chance = [r_prob,p_prob,s_prob][pick]/sum;
+	let error = 1 - corr_chance;
+	models.forEach(model => {
+		let pred = model.mod();
+		model.weight = model.weight + (pred[pick] - corr_chance) * error * model.weight
+	})
+	//console.log(models.map(e => e.name + ": " + e.weight));
+}
+
 function fight(a,b){
 	if(a === b){
 		return 0//tie
@@ -142,6 +319,7 @@ function fight(a,b){
 }
 
 function player_pick(pick,bot_choise){
+
 	level_0[pick]++;
 	if(turns){
 		level_1[prev1_player*3 + pick]++;
@@ -157,6 +335,9 @@ function player_pick(pick,bot_choise){
 	prev2_player = prev1_player;
 	prev1_player = pick;
 	prev1_bot = bot_choise;
+
+	update_models(pick);
+	update_stats(pick,bot_choise);
 
 	return fight(pick,bot_choise);
 }
@@ -181,6 +362,9 @@ function game_over(result){
 	}
 }
 
+const lead = 20;
+const rounds = 30;
+
 function start_game(){
 	game.style.display = "block";
 	controls.style.display = "block";
@@ -193,7 +377,8 @@ function start_game(){
 	let p_wins = create("span",false,player_wins,results);
 	create("span",false," Bot wins: ",results);
 	let b_wins = create("span",false,bot_wins,results);
-	create("span",false," (7 ahead needed to win): ",results);
+	//create("span",false," (" + lead + " ahead needed to win): ",results);
+	create("span",false," (" + rounds + " needed to win): ",results);
 
 	let rock = create("div",["rock","card","selectable"],false,game);
 	rock.title = "Rock";
@@ -214,12 +399,14 @@ function start_game(){
 	scissorsImage.src = "scissors.webp";
 
 	let player_card = create("div",["card"],false,arena);
+	create("p",false,"player",player_card);
 	player_card.title = "";
 	let player_cardImage = create("img",false,false,player_card);
 	player_cardImage.alt = "";
 	player_cardImage.src = "";
 
 	let bot_card = create("div",["card"],false,arena);
+	create("p",false,"bot",bot_card);
 	bot_card.title = "";
 	let bot_cardImage = create("img",false,false,bot_card);
 	bot_cardImage.alt = "";
@@ -254,7 +441,10 @@ function start_game(){
 			overlay.innerText = "Win!";
 			player_wins++;
 			p_wins.innerText = player_wins;
-			if(player_wins - 7 >= bot_wins){
+			/*if(player_wins - lead >= bot_wins){
+				game_over(1);
+			}*/
+			if(player_wins >= rounds){
 				game_over(1);
 			}
 		}
@@ -263,7 +453,10 @@ function start_game(){
 			overlay.innerText = "Loss!"
 			bot_wins++;
 			b_wins.innerText = bot_wins;
-			if(bot_wins - 7 >= player_wins){
+			/*if(bot_wins - lead >= player_wins){
+				game_over(0);
+			}*/
+			if(bot_wins >= rounds){
 				game_over(0);
 			}
 		}
@@ -302,7 +495,8 @@ function restart(){
 
 	create("h2","heading","Rules",instructions);
 	create("p","rule","You're playing Rock, Paper, Scissors against a bot",instructions);
-	create("p","rule","There will be multiple rounds. The game goes on until anyone has a lead of 7 wins",instructions);
+	//create("p","rule","There will be multiple rounds. The game goes on until anyone has a lead of " + lead + " wins",instructions);
+	create("p","rule","First to " + rounds + " rounds won wins the match",instructions);
 	create("p","rule","The bot doesn't make completely random decisions, it's trying to be clever",instructions);
 	create("p","rule","You can consistently beat the bot by being more clever",instructions);
 	create("hr",false,false,instructions);
